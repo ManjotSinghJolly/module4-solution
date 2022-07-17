d0a3d0b3af2b0c3176e0398f42cc745783f1c057
Manjot Jolly <manjotsinghjolly@gmail.com>
2022-07-17 13:54:35 +0530

    Completed the eniter exercise.

--- index.js
+++
@@ -1,9 +0,0 @@
-let array = ["Jonathan", "Nancy", "Steve", "Joyce", "Hopper"];
-
-array.map((el) => {
-  if (el.charAt(0) === "J" || el.charAt(0) === "j") {
-    console.log(`Goodbye ${el}`);
-  } else {
-    console.log(`Hello ${el}`);
-  }
-});
